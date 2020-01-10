a1fde8b84617ba93101e3cc94211909685e160e7
mupeng <123456>
2020-01-10 09:41:26 +0800

    首页中间tab切换

--- src/views/abyth/IndexPlatFrom.vue
+++ src/views/abyth/IndexPlatFrom.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="platfrom">
     <div class="system">
-      <div class="itemBtn"></div>
+      <div class="itemBtn">
+        <ul>
+          <li v-for="(item, index) in btnList" :key="index" @click="active(index)">
+            <a :href="item.url" target="_blank">
+              <div class="item">
+                <v-icon :name="item.icon" scale="3" />
+                <p>{{ item.name }}</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
       <div class="ehcartLine">
         <ve-line
           id="IndexLine"
@@ -9,6 +20,7 @@
           height="100%"
           :data="chartData"
           :extend="extend"
+          :grid="grid"
           :legend-visible="false"
           :title="chartTitle"
         ></ve-line>
@@ -21,8 +33,24 @@ export default {
   name: "IndexPlatFrom",
   data () {
     return {
+      btnList: [{
+        icon: 'globe',
+        name: '应急管理平台',
+        url: 'http://140.246.10.129:9081'
+      }, {
+        icon: 'chalkboard-teacher',
+        name: '远程培训',
+        url: 'https://test.atx.net.cn'
+      }, {
+        icon: 'fingerprint',
+        name: '电子考勤',
+        url: 'http://47.92.143.38:8089/kq/a/login'
+      }, {
+        icon: 'cart-plus',
+        name: '电子商城',
+        url: 'http://zs.atxmall.cn'
+      }],
       extend: {
-        'xAxis.0.axisLabel.rotate': 45,
         'series.0.smooth': false
       },
       chartData: {
@@ -37,7 +65,11 @@ export default {
         ]
       },
       chartTitle: {
-        text: '人流量统计'
+        text: '人流量统计',
+        top: '10px'
+      },
+      grid: {
+        bottom: '5%'
       }
     };
   }
@@ -54,11 +86,64 @@ export default {
     height: 100%;
 
     .itemBtn {
-      height: 30%;
+      height: 35%;
+
+      ul {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        overflow: hidden;
+
+        li {
+          width: 20%;
+          height: 90%;
+          cursor: pointer;
+
+          a {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            text-align: center;
+            justify-content: center;
+            align-items: center;
+            text-decoration: none;
+
+            .item {
+              color: #fff;
+            }
+          }
+
+          &:nth-child(1) {
+            background: #00c2ff;
+          }
+
+          &:nth-child(2) {
+            background: #00cddf;
+          }
+
+          &:nth-child(3) {
+            background: #24c768;
+          }
+
+          &:nth-child(4) {
+            background: #e5ce10;
+          }
+
+          &:nth-child(5) {
+            background: #ff7e00;
+          }
+
+          &:nth-child(5) {
+            background: #da0000;
+          }
+        }
+      }
     }
 
     .ehcartLine {
-      height: 70%;
+      height: 65%;
     }
   }
 }
